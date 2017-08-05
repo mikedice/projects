ee9d1dfe284522ea30c53cad5f61fe2cb87e0280
Mike Dice <mikedice417@hotmail.com>
2017-08-05 09:24:27 -0700

    updates

--- rpi-16-pixel-display/code/app.js
+++ rpi-16-pixel-display/code/app.js
@@ -1,25 +1,40 @@
 var i2c = require('i2c');
 var sleep = require('sleep');
 var address = 0x20;
+var AddressOfBankA =0x14;
 
 var wire = new i2c(address, {device: '/dev/i2c-1'});
 
-// each byte value in the array represents the 4 cathode and 4 anode bits
-// To turn a light on the cathode bit for the row has to go low and the anode bit 
-// for the column has to go high
-// so 1111 0000 would be all lights off
-//    0111 0001 would turn on row 1 column 1
-//    0111 0010 would turn on row 2 column 1
-//    0111 0100 would turn on row 3 column 1
-//    0111 1000 would turn on row 4 column 1
-// and so on and so forth
+// 8 bits are written to MCP23017 GPIO Bank A to control the lights. 
+// To turn on the row 1 column 1 (1,1) light make GPA0 high and GPA4 low. 
+// This will complete circuit through top-left light. 
 
-// turns all lights on
+// On the MCP23017 the right-most bit represents GPIO Bank A pin 0 (GPA0)
+// and the left-most bit represents GPIO Bank A pin 7 (GPA7). 
+// Handily, the bits in the digital word used to control the GPIO pins on the 
+// MCP23017 are arranged in the same order as the physical pins on the chip :)
+// With that in mind if we want to turn on row one column 1 (1,1)  we write
+// the following digital word to the MCP23017 Bank A
+//   1110 0001 (hex 0xE1)
+// This will cause pin GPA0 to be set to HIGH (+3.3V on a Raspbery Pi) 
+// and pin GPA4 to LOW (aka ground) the circuit through the light at (1,1)
+// will be completed and the light will shine bright like a diamond
+
+// If we want to turn on row 1 column 2 (1, 2) we write
+//    1110 0010 (hex 0xE2)
+// And for (1, 3)
+//    1110 0100 (hex 0xE4)
+// And for (1, 4)
+//    1110 1000 (hex 0xE8)
+// And for (2, 1)
+//    1101 0001 (hex 0xD1)
+// The table below shows the digital words you can use for
+// each light in the grid
 var data = [
-    0xE1,0xE2,0xE4,0xE8, // row 1
-    0xD1,0xD2,0xD4,0xD8, // row 2
-    0xB1,0xB2,0xB4,0xB8, // row 3
-    0x71,0x72,0x74,0x78  // row 4
+0xE1,0xE2,0xE4,0xE8, // row 1
+0xD1,0xD2,0xD4,0xD8, // row 2
+0xB1,0xB2,0xB4,0xB8, // row 3
+0x71,0x72,0x74,0x78  // row 4
 ];
 
 
@@ -61,7 +76,7 @@ console.log('wire created');
 
 function displayChar(chr, idx, duration, callback){
     // go through the pixels in each 'character' array and turn them on/off rapidly
-    wire.writeBytes(0x14, // addressing bank A of the mcp20317 io pins
+    wire.writeBytes(AddressOfBankA, // addressing bank A of the mcp20317 io pins
             [chr[idx]], // the character to write
             function(){ // callback after the write operation completes
                 sleep.usleep(750);
